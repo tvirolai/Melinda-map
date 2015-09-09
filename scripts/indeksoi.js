3b098b352a48c551cda9163608cef277ec2a47e0
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2015-09-09 20:38:21 +0300

    Indeksointi MongoDB:llä

--- scripts/indeksoi.js
+++ scripts/indeksoi.js
@@ -1,39 +1,25 @@
 /* jshint node:true */
 
+// Ohjelma lukee Aleph Sequential -muotoiset tiedostot (N = 73000) yksi kerrallaan
+// ja syöttää niiden bib-id:t ja otsikot kielittäin ja maittain MongoDB-kantaan
+
 'use strict';
 
 var walk = require('walk');
 var _ = require('underscore');
 var files = [];
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/indeksi';
+var writeCount = 0;
 
 var walker = walk.walk('./fin01', { followLinks: false });
 
 // formaatti:
-// {
-//     "languages": {
-//         "jpn": [
-//             {
-//                 "id": "013013",
-//                 "title": "kun toimeen tartuttiin"
-//             },
-//             {
-//                 "id": "12312",
-//                 "title": "Lodi dodi"
-//             }
-//         ]
-//     },
-//     "countries": {
-//         "fin": [
-//             {
-//                 "id": "02141",
-//                 "title": "Jopas jotakin"
-//             }
-//         ]
-//     }
-// }
+// {"id": 948729234, "country": "fi", "language": "fin", "title": "Kun toimeen tartuttiin"}
+// 
 
-var index = {"languages": {}, "countries": {} };
+var index = [];
 
 walker.on('file', function(root, stat, next) {
   root = root.slice(1);
@@ -46,7 +32,7 @@ walker.on('end', function() {
   getContent(files);
 });
 
-  var Record = function newRecord(id) {
+var Record = function newRecord(id) {
     this.id = id,
     this.language = '',
     this.country = '',
@@ -58,20 +44,18 @@ function filterList(files) {
   return _.filter(files, function(filename) { if (filename.slice(-4) === '.seq') return filename; });
 }
 
-
 function getContent(files) {
+  index = [];
   var file = files.pop();
   fs.readFile(file, 'utf-8', function(err, data) {
-    getContent(files);
     var fileContents = data.split('\n');
     indexContent(fileContents);
-    console.log(JSON.stringify(index));
+    saveToDB(index);
   });
 }
 
 function indexContent(content) {
   var currentRecord = new Record('');
-  //currentRecord = new Record('');
   for (var i = 0; i < content.length; i++) {
     var line = content[i];
     var lineID = line.slice(0, 9);
@@ -97,15 +81,10 @@ function indexContent(content) {
 }
 
 function indexRecord(record) {
-  if (!index.languages[record.language]) {
-    index.languages[record.language] = [];
+  if (record.supplement === 0) {
+    delete record.supplement;
+    index.push(record);
   }
-  if (!index.countries[record.country]) {
-    index.countries[record.country] = [];
-  }
-  var minimizedRecord = {"id": record.id, "title": record.title};
-  index.languages[record.language].push(minimizedRecord);
-  index.countries[record.country].push(minimizedRecord);
 }
 
 function recordLanguage(line) {
@@ -113,7 +92,7 @@ function recordLanguage(line) {
 }
 
 function recordTitle(line) {
-  return line.slice(18);
+  return line.slice(18).replace(/\$\$[A-Za-z]/g, ' ').trim();
 }
 
 function recordCountry(line) {
@@ -122,4 +101,20 @@ function recordCountry(line) {
     countryCode = countryCode.slice(0,2);
   }
   return countryCode;
+}
+
+function saveToDB(index) {
+  // database: indeksi, collection: data
+  MongoClient.connect(mongoUrl, function(err, db) {
+    if(err) throw err;
+    else {
+      //console.log("Successfully connected to " + mongoUrl + ".");
+      db.collection('data').insert(index, function(err, doc) {
+        writeCount += index.length;
+        console.log("Successfully inserted: " + writeCount);
+        db.close();
+        getContent(files);
+      });
+    }
+  });
 }
